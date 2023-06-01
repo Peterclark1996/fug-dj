712fe724909e9c7a8d9d4996aa72e8311d604be6
peterclark1996@gmail.com <peterclark1996@gmail.com>
2023-06-01 22:29:45 +0100

    Fix sizing of queued media to fit

--- client/src/routes/room/QueuePanel.tsx
+++ client/src/routes/room/QueuePanel.tsx
@@ -10,21 +10,21 @@ type QueuePanelProps = {
 
 const QueuePanel = ({ queue }: QueuePanelProps) => {
     return (
-        <div className="flex flex-col grow">
+        <div className="flex flex-col grow overflow-hidden">
             {queue.map(queuedMedia => (
                 <div
                     key={`${queuedMedia.userWhoQueued}_${queuedMedia.mediaId}`}
                     className={`${classes.outlinedText} flex flex-col relative rounded mx-2 mt-2 px-3 py-2 form-emboss outline outline-1 outline-slate-900 font-extrabold`}
                 >
                     <div className="flex items-start mb-1 z-10">
-                        <span className="rounded bg-slate-900/25 px-2">{queuedMedia.displayName}</span>
+                        <span className="rounded bg-slate-900/25 px-2 line-clamp-2">{queuedMedia.displayName}</span>
                     </div>
                     <div className="flex grow justify-between z-10">
-                        <span className="rounded bg-slate-900/25 px-2">
+                        <span className="rounded bg-slate-900/25 px-2 me-1 truncate">
                             <i className="fa-solid fa-user me-2" />
                             {queuedMedia.userWhoQueued}
                         </span>
-                        <span className="rounded bg-slate-900/25 px-2">
+                        <span className="flex items-center rounded bg-slate-900/25 px-2">
                             <i className="fa-solid fa-clock me-2" />
                             {moment
                                 .utc(queuedMedia.lengthInSeconds * 1000, "seconds")
