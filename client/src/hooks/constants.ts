550e1ac4323acf813ffd7385c35a8c16f3d3157b
Pete Clark <peterclark1996@gmail.com>
2023-06-12 22:29:34 +0100

    Connect via https

--- client/src/hooks/constants.ts
+++ client/src/hooks/constants.ts
@@ -2,5 +2,5 @@ export const getApiUrl = () => {
     if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
         return "http://localhost:8080/api"
     }
-    return `http://${window.location.host}/api`
+    return `https://${window.location.host}/api`
 }
